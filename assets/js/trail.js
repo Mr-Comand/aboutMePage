1669b890f3374d4e193a19753d1c12d56ad1a138
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-06-06 22:44:31 +0200

    Added mouse press detection and extracted sparkle creation into a separate function

--- assets/js/trail.js
+++ assets/js/trail.js
@@ -1,23 +1,42 @@
 const base_spread = 50
+
+let isMousePressed = false;
+
 document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('mousedown', function () {
+        isMousePressed = true;
+    });
+
+    document.addEventListener('mouseup', function () {
+        isMousePressed = false;
+    });
 
     document.addEventListener('mousemove', function (e) {
-
-        var spread = 50
-        const sparkle = document.createElement('div');
-        sparkle.className = 'sparkle';
-        sparkle.style.left = e.pageX + (Math.random() - 0.5) * base_spread + 'px';
-        sparkle.style.top = e.pageY + (Math.random() - 0.5) * base_spread + 'px';
-        sparkle.style.backgroundColor = generateWhiteWithHintOfColor();
-        document.body.appendChild(sparkle);
-        setTimeout(function () {
-            document.body.removeChild(sparkle);
-        }, 1000); // Adjust the duration of the sparkle effect
-
-
-
+        if (isMousePressed) {
+            createSparkle(e);
+        }
     });
 });
+
+
+
+function createSparkle(e) {
+
+    var spread = 50
+    const sparkle = document.createElement('div');
+    sparkle.className = 'sparkle';
+    sparkle.style.left = e.pageX + (Math.random() - 0.5) * base_spread + 'px';
+    sparkle.style.top = e.pageY + (Math.random() - 0.5) * base_spread + 'px';
+    sparkle.style.backgroundColor = generateWhiteWithHintOfColor();
+    document.body.appendChild(sparkle);
+    setTimeout(function () {
+        document.body.removeChild(sparkle);
+    }, 1000); // Adjust the duration of the sparkle effect
+
+
+
+}
+
 
 function generateWhiteWithHintOfColor() {
     // Hue: 0-360 (covers all colors)
