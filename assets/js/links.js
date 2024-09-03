15ccd693ccc44ce9d8fc9d60d7b4b9d20c81c635
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-09-03 14:04:34 +0200

    added functionality to disable links if disableLinks is set as a URLParameter

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,13 +1,21 @@
-        // Get all anchor elements in the document
-        var allLinks = document.getElementsByClassName('link');
+// Function to get URL parameter value
+function getURLParameter(name) {
+    const regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)', 'i');
+    const match = regex.exec(window.location.search);
+    return match ? decodeURIComponent(match[1]) : null;
+}
+if (!getURLParameter('disableLinks')) {
+    // Get all anchor elements in the document
+    var allLinks = document.getElementsByClassName('link');
 
-        // Iterate over each anchor element
-        for (var i = 0; i < allLinks.length; i++) {
-            // Attach mousedown event listener to the anchor element
-            allLinks[i].addEventListener("mouseup", function (event) {
-                // Start the press timer
-                if ((new Date().getTime() - clickTime) < 100) {
-                    window.location.href = this.getAttribute("target");
-                }
-            });
-        }
+    // Iterate over each anchor element
+    for (var i = 0; i < allLinks.length; i++) {
+        // Attach mousedown event listener to the anchor element
+        allLinks[i].addEventListener("mouseup", function (event) {
+            // Start the press timer
+            if ((new Date().getTime() - clickTime) < 100) {
+                window.location.href = this.getAttribute("target");
+            }
+        });
+    }
+}
